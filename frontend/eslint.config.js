d7ddb012ae42052c70db9164deaeb8d985517771
Rinki-S <iam@rinki.top>
2026-06-06 00:35:36 +0800

    chore(frontend): add eslint and prettier config

---
+++ frontend/eslint.config.js
@@ -0,0 +1,29 @@
+import eslintConfigPrettier from "eslint-config-prettier";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  {
+    ignores: ["build", ".react-router", "node_modules"],
+  },
+  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.{ts,tsx}"],
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        {
+          allowConstantExport: true,
+          allowExportNames: ["ErrorBoundary", "Layout", "links", "meta"],
+        },
+      ],
+    },
+  },
+  eslintConfigPrettier,
+);
